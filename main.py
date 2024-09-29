d0b4157ad7541209f6d2f093d8756c1d17d23417
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-09-29 17:18:16 -0400

    streamlit updated

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ load_dotenv()
 
 st.set_page_config(page_title='Ask Questions about the Christian Faith')
 
-# st.sidebar.title('Ask Questions about the Christian Faith')
-# llm_name = st.sidebar.selectbox('Language model', ['mistral.mixtral-8x7b-instruct-v0:1'])
-# max_tokens = st.sidebar.number_input('max_tokens', min_value=0, value=4096)
-# temperature = st.sidebar.number_input('temperature', min_value=0.0, value=0.7)
-# top_p = st.sidebar.number_input('top_p', min_value=0.0, max_value=1.0, value=0.8)
+st.sidebar.title('Ask Questions about the Christian Faith')
+llm_name = st.sidebar.selectbox('Language model', ['mistral.mistral-7b-instruct-v0:2'])
+max_tokens = st.sidebar.number_input('max_tokens', min_value=0, value=4096)
+temperature = st.sidebar.number_input('temperature', min_value=0.0, value=0.7)
+top_p = st.sidebar.number_input('top_p', min_value=0.0, max_value=1.0, value=0.8)
 
 question = st.text_area('Your Question')
 
 if st.button('Ask!'):
     payload = json.dumps({
         'question': question,
-        # 'llm_name': llm_name,
-        # 'llm_config': {
-        #     'max_tokens': max_tokens,
-        #     'temperature': temperature,
-        #     'top_p': top_p
-        # },
+        'llm_name': llm_name,
+        'llm_config': {
+            'max_tokens': max_tokens,
+            'temperature': temperature,
+            'top_p': top_p
+        },
         'source': 'streamlit'
     })
 
